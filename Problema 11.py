aaee9c79d9e3cfb4afea144d954b3fb36cf0d8b9
AxentiAndrei2004 <71274413+AxentiAndrei2004@users.noreply.github.com>
2020-10-05 22:37:52 +0300

    Add files via upload

---
+++ Problema 11.py
@@ -0,0 +1,4 @@
+a=int(input('Numarul de iepuri de la inceputul lunii='))
+b=int(input('Numarul de iepuri care au murit='))
+c=int(input('Numarul de iepuri care s-au nascut='))
+print('Numarul total de iepuri este',a-b+c)
