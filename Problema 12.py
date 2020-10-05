1a93499237ef00de6fd006af7b90052c492a97f3
AxentiAndrei2004 <71274413+AxentiAndrei2004@users.noreply.github.com>
2020-10-05 22:47:36 +0300

    Add files via upload

---
+++ Problema 12.py
@@ -0,0 +1,5 @@
+a=int(input('Numarul de oua pe care o da prima gaina timp de o zi='))
+b=int(input('Numarul de oua pe care o da a doua gaina timp de o zi='))
+c=int(input('Numarul de oua pe care o da a treia gaina timp de o zi='))
+d=int(input('Numarul de oua pe care o da a patra gaina timp de o zi='))
+print('Timp de o saptamana s-au obtinut',(a+b+c+d)*7,'oua')
